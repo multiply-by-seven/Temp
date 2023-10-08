3f52dd4a1d919cce759b24c1e380ad50a683a8a6
lucky <1421191417@qq.com>
2023-10-08 10:36:10 +0800

    py3.11

---
+++ ks_py3.11/alpine_aarch64/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.3.10 (trial), 000000, 2023-10-08T10:32:15.949715
+from .pyarmor_runtime import __pyarmor__
---
+++ ks_py3.11/alpine_x64/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.3.10 (trial), 000000, 2023-10-08T10:31:57.826459
+from .pyarmor_runtime import __pyarmor__
